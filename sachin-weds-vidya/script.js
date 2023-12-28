fa22603344b95f3225522d5e494caf495fb555c2
Sachin Kn <106311617+sachinknsachi@users.noreply.github.com>
2023-12-28 23:59:30 +0530

    Update script.js

--- sachin-weds-vidya/script.js
+++ sachin-weds-vidya/script.js
@@ -1,13 +1,16 @@
 function toggleLang() {
     var langSwitch = document.getElementById('lang-switch');
-    var enText = document.querySelector('.en-text');
-    var knText = document.querySelector('.kn-text');
-
-    if (langSwitch.checked) {
-        enText.style.display = 'none';
-        knText.style.display = 'block';
-    } else {
-        enText.style.display = 'block';
-        knText.style.display = 'none';
+    var enText = document.getElementsByClassName('en-text');
+    var knText = document.getElementsByClassName('kn-text');
+    if (enText.length === knText.length) {
+        for (var i = 0; i < enText.length; i++) {
+            if (langSwitch.checked) {
+                enText[i].style.display = 'none';
+                knText[i].style.display = 'flex';
+            } else {
+                enText[i].style.display = 'flex';
+                knText[i].style.display = 'none';
+            }
+        }
     }
 }
